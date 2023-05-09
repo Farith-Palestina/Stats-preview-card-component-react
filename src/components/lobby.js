05a370da2a5f90922ebe3489a4cbc0be4ef8c5bf
Farith Palestina <palestinagodinez@gmail.com>
2023-05-09 12:38:15 -0600

    Final page

---
+++ src/components/lobby.js
@@ -0,0 +1,67 @@
+function Lobby(){
+    return(
+        <div className='allpage'>
+            <div className='container columns-container'>
+                <div className='data-container'>
+                    <div className='data-title-container'>
+                        <p className='data-title'>
+                            Get <span>insights</span> that help your business grow.
+                        </p>
+                    </div>
+                    <div className='data-main-paragraph-container'>
+                        <p className='data-main-paragraph'>
+                            Discover the benefits of data analytics and make better decisions regarding revenue, customer experience, and overall efficiency.
+                        </p>
+                    </div>
+                    <div className='data-statistics-container columns-data-statistics'>
+                        <div className='data-companies'>
+                            <div className='data-number'>
+                                <p>
+                                    10k+
+                                </p>
+                            </div>
+                            <div className='data-text'>
+                                <p>
+                                    COMPANIES
+                                </p>
+                            </div>
+                        </div>
+                        <div className='data-templates'>
+                            <div className='data-number'>
+                                <p>
+                                    314
+                                </p>
+                            </div>
+                            <div className='data-text'>
+                                <p>
+                                    TEMPLATES
+                                </p>
+                            </div>
+                        </div>
+                        <div className='data-queries'>
+                            <div className='data-number'>
+                                <p>
+                                    12M+
+                                </p>
+                            </div>
+                            <div className='data-text'>
+                                <p>
+                                    QUERIES
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className='images-container'>
+                    <div className='color-image-container'>
+                    </div>
+                </div>
+            </div>
+            <div className='footer-page'>
+                <p>Created by <a href="https://github.com/Farith-Palestina">Farith Palestina</a>.</p>
+            </div>
+        </div>
+    );
+}
+
+export default Lobby;
